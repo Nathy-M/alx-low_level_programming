01e641fc78d142a7b4aef78930b211e71a8937d9
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-11 01:17:44 -0800

    new commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,17 +15,17 @@ int main(void)
 	/* your code goes there*/
 	if(n > 0)
 	{
-		printf("%dis positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	
 	if(n == 0)
 	{
-		printf("%dis zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	
 	if(n < 0)
 	{
-		printf("%dis negative\n", n);
+		printf("%d is negative\n", n);
 	}
 
 	return (0);
