5a8641743e5f5f78abd4721ff33fd1b49be77bd9
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-14 17:33:36 +0000

    new commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - progarm that prints an output by using the function puts
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0); }	
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-the fourth one
+hello world
