c7f1fa68fe76321edfce88826b5bc3bfb6b51c7d
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-15 12:31:21 +0000

    main library containing defined functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,9 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - The entry point of a program
  *
  * Return: returns success
  */
-int main(void)
-{
-	print_alphabet();
-	return (0); }
-
 void print_alphabet(void)
 {
 	char letter;
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File: main.h
+ *
+ * description: header file
+ */
+int _putchar(char c);
+void print_alphabet(void);
+
+
+#endif
