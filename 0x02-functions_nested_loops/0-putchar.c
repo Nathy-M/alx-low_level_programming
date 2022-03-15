d0a8ecd1199d2d4e8dc0e4fc301a31be5d3ab81d
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-15 11:22:21 +0000

    New commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**_putchar - writes thr character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+	putchar('_putchar');
+return (0); }	
