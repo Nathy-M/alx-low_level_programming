727a0e5dd51815ee4563f5e81d7b7e5975bd0dfa
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-15 15:09:45 +0000

    printing last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,12 @@ int print_last_digit(int num)
 {
 	int last_digit = num % 10;
 	
-	if (last_digit == num % 10)
+	if (last_digit < num % 10)
 	{
+		last_digit *= -1;
+		_putchar(last_digit + '0');
 		return (last_digit);
 	}
+	else
+		return (last_digit);
 }
