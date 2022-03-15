c767f10b63566b7c99eb0402e0a22229bc3771ae
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-15 12:47:17 +0000

    1-alphabet commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
- * main - The entry point of a program
- *
- * Return: returns success
+ *print_alphabet - a function that prints alphabet in lower case letters
  */
 void print_alphabet(void)
 {
 	char letter;
-	
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
