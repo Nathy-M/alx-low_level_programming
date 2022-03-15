d9072f58bcdadcb93169ecb2a77319516fe521f8
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-15 11:45:20 +0000

    New commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
-#include <main.h>
-#include <unistd.h>
+#include "main.h"
+#include "unistd.h"
 /**_putchar - writes thr character c to stdout
  * @c: The character to print
  *
@@ -8,7 +8,14 @@
  */
 int main(void)
 {
-	_putchar('_putchar');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 return (0); }	
