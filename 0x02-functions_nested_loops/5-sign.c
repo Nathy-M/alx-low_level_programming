b8cd2a71e29091ef717c536df5808dc81d2e85d5
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-15 16:50:31 +0000

    new commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,8 +3,8 @@
  * print_sign - return 1 and prints + if n is greater than zero
  *                          return 0 and prints 0 if n is zero
  *                          returns -1 and prints - if n is lessthan zero
- * @n: number to be checked 
- * Return: returns 0 for success   
+ * @n:number to be checked 
+ * Return:returns 0 for success   
  */
 int print_sign(int n)
 {
@@ -18,7 +18,7 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else 
+	else
 	{
 		_putchar('-');
 		return (-1);
