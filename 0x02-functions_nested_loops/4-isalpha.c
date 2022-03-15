6ef90c67338493e0d17e422e97e3a093bc3c0d90
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-15 16:55:51 +0000

    new commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /*
- * int _isalpha(int c) -- checking for upper, lower and letter
+ * isalpha -- checking for upper, lower and letter
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || ( c >= 'A' && c <= 'Z'))
+	if (( c >= 'a' && c <= 'z') || ( c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
