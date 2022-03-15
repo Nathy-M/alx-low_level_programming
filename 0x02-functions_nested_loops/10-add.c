662e5d05e79b337c5a2a536438d133c536164992
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-15 16:02:07 +0000

    new commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * add -- function that adds two numbers and returns the value
- * @num1 - the first number to be added
- * @num2 - the second number to be added
+ * @num1:  the first number to be added
+ * @num2:  the second number to be added
  *
  * Return: Returns the value of the two sums
  */
-int add( int num1, int num2)
+int add ( int num1, int num2)
 {
 	return (num1 + num2);
 }
