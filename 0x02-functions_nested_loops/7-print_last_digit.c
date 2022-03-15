80c316a48851771f8974e3096c7731fa29efb7a2
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-15 14:52:38 +0000

    printing last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * int print_last_digit(int) -- returns value of last digit
+ */
+int print_last_digit(int num)
+{
+	int last_digit = num % 10;
+	
+	if (last_digit == num % 10)
+	{
+		return (last_digit);
+	}
+}
