df26a4fd1abbdc4854f781dbdad6e65395d28bdc
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-16 16:45:44 +0000

    new commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int num1 = 0;
+	int num2 = 1;
+	int num3;
+	int nextTerm;
+
+	nextTerm = num1 +num2;
+
+	for (num3 = 3; num3 <= 50; num3++)
+	{
+		printf("%d, ", nextTerm);
+
+		num1 = num2;
+		num2 = nextTerm;
+
+		if (num3 == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return 0;
+}
