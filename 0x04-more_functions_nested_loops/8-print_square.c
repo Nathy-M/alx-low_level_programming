43b1928e8485d33ea0f457a28b698d7723abd782
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-17 19:12:17 +0000

    new commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - prints square by using #
+ *@size - size of square
+ * Return: returns square
+ */
+void print_square(int size)
+{
+	int num;
+	int num1;
+
+	if (size > 0)
+	{
+
+	for (num = 0; num <= size; num++)
+	{
+		for (num1 = 0; num1 < size; num1++)
+		{
+			_putchar('#');
+		}
+		if (num == size - 1)
+			continue;
+		_putchar('\n');
+	}
+
+	}
+	_putchar('\n');
+}
