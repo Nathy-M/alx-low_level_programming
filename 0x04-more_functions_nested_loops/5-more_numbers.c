af10b22ef46a0240a672d2946f78fc02d5b944e0
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-17 16:32:16 +0000

    new commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * more_numbers - prints number from 0 to 14
+ * ten times
+ */
+void more_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+	for (num = 1; num <= 5; num++)
+		_putchar((num % 10) + '9');
+	_putchar('\n');
+}
