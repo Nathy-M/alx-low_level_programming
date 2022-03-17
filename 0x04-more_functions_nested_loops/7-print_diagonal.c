ceefdf85bd54f1fc7206535b55061a10ac5aa5dc
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-17 17:25:11 +0000

    new commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - prints \ in diagonal
+ * @n: passing number of times to be printed
+ */
+void print_diagonal(int n)
+{
+	int num;
+	int count;
+
+	for (num = 0; num < n; num++)
+	{
+		for (count = 0; count < num; count++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+
+		if (num == n - 1)
+		{
+			continue;
+		}
+	}
+	_putchar('\n');
+}
