189f073e3a26fe80cda46f88f2c9c9b92bec9bf9
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-17 15:56:08 +0000

    new commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit between 0 and 9
+ * @c - digit to be checked
+ * Return: returns 0 on success
+ */
+int _isdigit(int c)
+{
+	if ((c > 0) && (c <= 9))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
