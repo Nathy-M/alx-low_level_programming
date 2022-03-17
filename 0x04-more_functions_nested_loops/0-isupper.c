c0faabf873aa94c7391b8f7ed5c1a4fff5de3457
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-17 15:51:48 +0000

    new commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - return 1 for uppercase letters
+ * returns 0 for lower case letters
+ * @c: letter to be checked
+ * Return: return 0 on sucess
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
