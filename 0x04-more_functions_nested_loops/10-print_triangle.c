2f3d8c9834c814a70a71d8e6363246e8bc8b9d83
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-18 09:02:06 +0000

    new commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -18,7 +18,7 @@ void print_triangle(int size)
 	{
 		for (height = 1; height <= size; height++)
 		{
-			for (length = size - 1; length > 0; length--)
+			for (length = size - height; length > 0; length--)
 			{
 				_putchar(' ');
 			}
