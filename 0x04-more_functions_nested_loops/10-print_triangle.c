d5aa3c64dbfc312ed2d53805cd756c216ac73613
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-18 08:46:08 +0000

    new commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle
+ * @size: size of triangle
+ * Return: returns 0
+ */
+void print_triangle(int size)
+{
+	int height;
+	int length;
+	int i;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (height = 1; height <= size; height++)
+		{
+			for (length = size - 1; length > 0; length++)
+			{
+				_putchar(' ');
+			}
+			for (i = 1; i <= height; i++)
+			{
+				_putchar('*');
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
