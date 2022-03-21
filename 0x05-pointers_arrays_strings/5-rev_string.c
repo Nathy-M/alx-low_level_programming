9a75187832294fa6ae2996c761aff35e65a53b98
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-21 22:32:25 +0000

    new commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,6 +6,17 @@
  */
 void rev_string(char *s)
 {
-	strrev(s);
+	int length;
+	int count;
+	char temp;
 
+	while(s[length++])
+		count++;
+
+	for (length = count - 1; length >= count / 2; length--)
+	{
+		temp = s[length];
+		s[length] = s[count - length - 1];
+		s[count - length - 1] = temp;
+       	}
 }
