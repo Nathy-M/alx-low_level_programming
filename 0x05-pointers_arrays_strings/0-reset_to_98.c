d2d4b2c57fbfbb0777a43a001d8fc9df200e1cd2
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-21 18:07:00 +0000

    new commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int and
+ * upadates the value it points to 98
+ * @n: the pointer to an int
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
