125de519be3fc90d9b048ef1c12f0b3f2965d6eb
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-21 22:13:25 +0000

    new commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - function that copies the string
+ * @dest: destination point
+ * @src: source
+ * Return: returns char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+}
