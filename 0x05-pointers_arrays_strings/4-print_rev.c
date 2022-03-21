9d3985c0f5c9b163a0774eedbd8500b9f219981d
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-21 19:45:43 +0000

    new commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - prints string in reverse direction
+ * @s: passed string
+ */
+void print_rev(char *s)
+{
+	char *temp_str;
+	int count;
+
+	for (count = s.length() - 1; count > 0; count--)
+	{
+		*temp_str = *s(count);
+	}
+}
