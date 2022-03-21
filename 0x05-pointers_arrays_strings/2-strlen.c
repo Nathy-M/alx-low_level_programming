0746be8bc792efa909ed72c39603a535031199bd
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-21 18:24:52 +0000

    new commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - returns length of string
+ * @s: a charcter argument
+ * Return: returns string length
+ */
+int _strlen(char *s)
+{
+	int str_len;
+
+	str_len = strlen(*s);
+	return str_len;
+}
