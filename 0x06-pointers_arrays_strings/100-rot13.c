7b2a24fa199ba4f5ddd83ee63b8fe06d052cc7ea
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-24 17:09:21 +0000

    new commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rot13 - rotates every alphabet by 13
+ * @str: string to be rotated
+ * Return: returns pointer to str
+ */
+char *rot13(char *str)
+{
+	int num = 0;
+
+	while (str[num])
+	{
+		for (str[num] = 'a'; str[num] = 'z'; str[num]++)
+		{
+			str[num] += 13;
+			if (str[num] >= 'A' && str[num] <= 'Z')
+				str[num] += 13;
+		}
+		num++;
+	}
+	return (str);
+}
