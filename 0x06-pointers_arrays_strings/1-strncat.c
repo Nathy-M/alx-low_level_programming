a754bd6c7fdb949b2ac09d68492c965de6752130
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-24 08:11:59 +0000

    new commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,6 +5,7 @@
  * @dest: destination for the source stirng
  * @src: string to be appended
  * @n: maximum number of charater to be appended
+ * Return: a pointer to the resulting string
  */
 char *_strncat(char *dest, char *src, int n)
 {
