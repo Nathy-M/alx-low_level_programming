85b6af8146e80a42746196290efdd97e1be0d9b4
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-24 08:20:53 +0000

    new commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - function that copies a string
+ * @dest: pointer to the destination array where the content is to be copied
+ * @src: string to be copied
+ * @n: number of character to be copied from the source
+ * Return: pointer to the destination array
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
