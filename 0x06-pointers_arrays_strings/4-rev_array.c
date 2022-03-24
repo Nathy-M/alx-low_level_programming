5f228ca4814d4d1beb5bc46f4daa9b72e8732b0e
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-24 11:09:42 +0000

    new commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,13 @@ void reverse_array(int *a, int n)
 	int num;
 	int tmp;
 
-	for (num = n - 1; num > n / 2; num++)
+	for (num = n - 1; num >= n / 2; num++)
 	{
 		temp = a[n - 1 - num];
 		a[n - 1 - num] = a[num];
 		a[num] = tmp;
+		_putchar(',');
+		_putchar(' ');
+
 	}
 }
