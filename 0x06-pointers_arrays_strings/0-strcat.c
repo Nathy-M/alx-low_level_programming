fc8737a8609b6bf50d187f8ede366ee6da50b18e
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-24 07:21:17 +0000

    new commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - function that concatenate two strings
+ * @dest: destination string
+ * @src: source string
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+}
