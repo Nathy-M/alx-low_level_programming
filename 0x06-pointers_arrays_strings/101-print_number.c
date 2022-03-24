2bc12db0cc3b08bd1797664b9432dbb2a985576e
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-24 17:31:09 +0000

    new commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,5 +5,14 @@
  */
 void print_number(int n)
 {
-	_putchar(n);
+	int num = n;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
 }
