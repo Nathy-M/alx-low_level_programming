123452f0f923413ce92732bfd2e69815e09b64da
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-28 09:33:03 +0000

    commit for 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - function used to copy a block of data from a source address
+ * to a destination address
+ * @dest: destination character pointer
+ * @src: source character pointer
+ * @n: number of characters to be copied
+ *
+ * Return: returns pointer to destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int count;
+
+	for (count = 0; count < n; count++)
+	{
+		dest[count] = src[count];
+	}
+	return (dest);
+}
