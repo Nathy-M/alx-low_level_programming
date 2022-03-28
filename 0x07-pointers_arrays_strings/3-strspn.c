77ea06cb44963e37bc00eea824a10f9b56f202ab
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-28 22:19:39 +0000

    new commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,6 +10,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	strspn(*s, *accept);
-	return (s);
+	unsigned int bytes = 0;
+	int count;
+
+	while (*s)
+	{
+		for (count = 0; accept[count]; count++)
+		{
+			if (*s == accept[count])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[count + 1] == '\0')
+				return (bytes);
+			s++;
+		}
+	}
+	return (bytes);
 }
