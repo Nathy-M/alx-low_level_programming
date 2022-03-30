12c99aec21e9c0e376095afb697118bb0a198722
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-30 16:20:19 +0000

    new commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _puts_recursion - function that prints a string follwed by a new line
+ * @s: string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+	puts('\n');
+}
