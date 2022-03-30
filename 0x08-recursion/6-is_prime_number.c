c32d2c1a6adb0533d3a96a04c849dca92635d3dd
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-30 19:23:21 +0000

    new commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * is_prime_number - function that finds prime number.
+ * @n: number to be computed
+ *
+ * Return: returns 1 if number is prime.
+ *         otherwise returns 0
+ */
+
+int is_prime_number(int n)
+{
+	int div = 2;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n >= 2 && n <= 3)
+	{
+		return (1);
+	}
+	return (is_prime(n, div));
+}
+int is_prime(int num, int div)
+{
+	if (num % div == 0)
+	{
+		return (0);
+	}
+	if (div = num / 2)
+	{
+		return (1);
+	}
+	return (is_prime(num, div + 1));
+}
