db57ef014b145c1a44fc32ce97ad29735815028b
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-30 17:22:25 +0000

    new commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,10 +10,11 @@
 
 int _strlen_recursion(char *s)
 {
-	int str_len;
 
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		return 1 + _strlen_recursion(s + 1);
+		return 0;
 	}
+	else
+		return 1 + _strlen_recursion(s + 1);
 }
