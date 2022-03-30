f92f35cb3307fc83aeba4384a83fb78e946bd777
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-30 18:44:27 +0000

    new commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,5 +12,37 @@
 
 int _sqrt_recursion(int n)
 {
-	return (sqrt(n));
+	int llimit;
+	int rlimit = n;
+	int _sqrt;
+	int mid;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if(n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+	while (llimit <= rlimit)
+	{
+		mid = llimit + (rlimit - llimit) / 2;
+		if (mid * mid == n)
+		{
+			return mid;
+		}
+		else if (mid >= n / mid)
+		{
+			rlimit = mid - 1;
+		}
+		else if (mid <= n / mid)
+		{
+			llimit = mid + 1;
+			_sqrt = mid;
+		}
+	}
+	return (_sqrt);
 }
