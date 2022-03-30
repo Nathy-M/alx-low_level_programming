28d2cf80fd650d36e1345cb3cf7fbb31d8d12dc0
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-30 19:37:26 +0000

    new commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,12 +6,9 @@
  * Return: returns 1 if number is prime.
  *         otherwise returns 0
  */
-int is_prime(int num, int div);
-int is_prime_number(int n);
-
 int is_prime_number(int n)
 {
-	int div = 2;
+	int div = num / 2;
 
 	if (n <= 1)
 	{
@@ -21,17 +18,28 @@ int is_prime_number(int n)
 	{
 		return (1);
 	}
-	return (is_prime(n, div));
+	else
+	{
+		is_prime(num, div);
+	}
 }
 int is_prime(int num, int div)
 {
-	if (num % div == 0)
-	{
-		return (0);
-	}
-	if (div = num / 2)
+	if (div == 1)
 	{
 		return (1);
 	}
-	return (is_prime(num, div + 1));
+	else
+	{
+		if (num % div == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			is_prime (num, div - 1);
+
+		}
+	}
+	return (is_prime(num, div - 1));
 }
