794b656651db747441b1a03830304ebeb82501b0
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-30 16:58:56 +0000

    new commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen_recursion - function that returns length of string.
+ * @s: string for which the length is computed.
+ *
+ * Return: returns length of string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int str_len;
+
+	if (*s != '\0')
+	{
+		str_len = _strlen_recursion(s + 1);
+	}
+	_putchar(str_len);
+}
