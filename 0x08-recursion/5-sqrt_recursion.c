4946d8babb3a02238b61caf867d4aac16d5451f7
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-03-31 16:33:39 +0000

    new commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,42 +4,41 @@
 /**
  * _sqrt_recursion - function that returns the natural square root of
  * a number.
- * @n: number to be computed.
+ * @n: number to be computed
  *
- * Return: returns -1 if the number doesnot have natural square root
- *         else returns the square root of a number.
+ * Return: returns -1 if n is less than 0.
+ *         returns 0 if n is equal to 0.
+ *         returns square root of n for number different from the above one.
  */
 
 int _sqrt_recursion(int n)
 {
-	int llimit = 1;
-	int rlimit = n;
-	int _sqrt;
-	int mid;
+	int start = 1;
+	int end = n;
+	int ans;
 
 	if (n < 0)
 	{
 		return (-1);
 	}
-	else if(n == 0)
+	else if (n == 0)
 	{
 		return (0);
 	}
-	else
+	
+	while (start <= end)
 	{
-	while (llimit <= rlimit)
-	{
-		mid = llimit + (rlimit - llimit) / 2;
+		int mid = (start + (end - start) / 2);
 
 		if (mid <= (n / mid))
 		{
-			llimit = mid + 1;
-			_sqrt = mid;
+			ans = mid;
+			start = mid + 1;
 		}
 		else
 		{
-			rlimit = mid - 1;
+			end = mid - 1;
 		}
 	}
-	return (_sqrt);
+	return (ans);
 }
