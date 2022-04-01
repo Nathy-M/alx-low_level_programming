cce6eb0b2cea71f8ed1d3a06a4d3eb03b6ada8f6
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-04-01 17:07:50 +0000

    new commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: number of command line arguments
+ * @argv: an array containing the program command line argument
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
