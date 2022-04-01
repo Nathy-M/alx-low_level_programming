1805e91410eb9af9def4d4db234f9758583b2aa6
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-04-01 17:14:01 +0000

    new commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - main function
  * @argc: number of command line arguments
@@ -10,6 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	printf("%d\n", argc);
+	if (argc > 0)
+		printf("%d\n", argc - 1);
 	return (0);
-}
+ }
