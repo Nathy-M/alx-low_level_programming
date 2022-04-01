2465d83e43b28f3062ff44a752e251dadee37edb
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-04-01 16:48:29 +0000

    new commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints program name.
+ * @argc: represent number of the command line entered while running the program.
+ * @argv: an array of pointers to the strings.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	_putchar(argv[0]);
+	_putchar('\n');
+}
