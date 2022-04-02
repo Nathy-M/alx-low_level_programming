ad82654e5fb36a3fbe5d719e4ca0d548ba4b7e1b
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-04-02 15:28:21 +0000

    new commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: size of the argv array
+ * @argv: an array of size argc
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc < 1)
+	{
+		printf("0\n");
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if ((argv[i] >= 65 && argv[i] <= 90) || (argv[i] >= 97 && argv[i] <= 122))
+		{
+			printf("Error\n");
+		}
+		else
+		{
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+}
