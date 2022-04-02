448a08ab74b5294cea6a124e8f8fd08405dc7a25
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-04-02 19:16:25 +0000

    new commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,12 +12,12 @@ int main(int argc, char *argv[])
 	int cents;
 	int c = 0;
 
-	if (argc != 1)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi(argv[argc]);
+	cents = atoi(argv[1]);
 
 	if (cents < 0)
 	{
