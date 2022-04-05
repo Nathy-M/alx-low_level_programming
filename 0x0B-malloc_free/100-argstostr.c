c6bd04e0cde5bc2572f3f00bd4452c24c6cc86c5
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-04-05 21:42:56 +0000

    new commit

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * argstostr - function that concatenates all the arguments
+ * @ac: the first argument.
+ * @av: the second argument.
+ *
+ * Return: returns a pointer to a new string
+ *         or NULL if fails.
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *args;
+	int i, j, arg, count;
+
+	if (ac == 0 || av == 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			count++;
+		}
+	}
+
+	args = malloc(sizeof(char) * (count + 1));
+
+	if (args == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			args[arg] = av[i][j];
+		}
+		args[arg] = '\n';
+	}
+	args[count] = '\0';
+
+	return (args);
+}
