50ef1405ee08cf6ac2ac184ad282935c9a224fa8
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-04-07 20:29:45 +0000

    new commit

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - function that reallocates memory using malloc and free
+ * @ptr: pointer to the memory allocated with old_size.
+ * @old_size: size for ptr
+ * @new_size: new size for the new memory block
+ *
+ * Return: If new_size == old_size - ptr.
+ *         If new_size == 0 and ptr is not NULL - NULL.
+ *         Otherwise - a pointer to the reallocated memory block.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *ralloc;
+	unsigned int i;
+	char *ptr_cpy, *new;
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		ralloc = malloc(new_size);
+
+		if (ralloc == NULL)
+		{
+			return (NULL);
+		}
+		return (ralloc);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr_cpy = ptr;
+	ralloc = malloc(sizeof(*ptr_cpy) * new_size);
+	if (ralloc == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	for (i = 0; i < old_size && i < new_size; i++)
+	{
+		new[i] = *ptr_cpy++;
+	}
+	free(ptr);
+	return (ralloc);
+}
