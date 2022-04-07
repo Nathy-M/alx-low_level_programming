3c6ec9ea6eedae62b74f7bf153552895458c20e8
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-04-07 16:22:03 +0000

    new commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,6 +4,10 @@
 
 /**
  * malloc_checked - a function that allocates memory using malloc.
+ * @b: memory to be allocated
+ *
+ * Return: returns a pointer to an aloctaed memmory.
+ * If malloc fails, the function terminates the process by status value of 98
  */
 void *malloc_checked(unsigned int b)
 {
@@ -15,5 +19,4 @@ void *malloc_checked(unsigned int b)
 	{
 		exit(98);
 	}
-	return (mal);
 }
