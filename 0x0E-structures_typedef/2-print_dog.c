b05adf39dd3204c917b5a858c833a4a68dc04e28
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-04-11 17:47:34 +0000

    new commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,7 +19,7 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf("Name: %s\n", d->name;
+		printf("Name: %s\n", d->name);
 	}
 	if (d->age <= 0)
 	{
@@ -27,12 +27,14 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-	printf("Age: %f\n", d->age);
+		printf("Age: %f\n", d->age);
+	}
 	if (d->owner == NULL)
 	{
 		printf("Owner: (nil)\n");
 	}
 	else
 	{
-	printf("Owner: %s\n", d->owner);
+		printf("Owner: %s\n", d->owner);
+	}
 }
