7701471747a3d9c5a1670596170687eef07d3bb3
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-04-14 17:17:26 +0000

    new commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <varargs.h>
+
+/**
+ * sum_them_all - function that returns the sum of all its parameters
+ * @n: number of elements to be added
+ *
+ * Return: sum of the numbers
+ * else if n == 0, return 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	const unsigned int i;
+	unsigned int sum;
+	va_list ap;
+
+	va_start (ap, n);
+
+	sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg (ap, int);
+	}
+
+	va_end (ap);
+	return (sum);
+}
