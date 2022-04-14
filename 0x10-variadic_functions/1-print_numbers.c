36e1c0b24a2c1df88dcebf90221b01c12b8c0592
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-04-14 18:08:03 +0000

    new commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,9 +15,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(sp, n);
 	for (val = 0; val < n; val++)
 	{
-		printf("%d\n", va_arg(sp, int);
-			if (val != (n - 1) && separator != NULL)
+		printf("%d", va_arg(sp, int));
+
+		if (val != (n - 1) && separator != NULL)
+		{
 			printf("%s", separator);
+		}
 	}
 	printf("\n");
 	va_end(sp);
