9377de73edc1a3b2880395dc35821989a4434574
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-04-14 18:28:00 +0000

    new commit

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.c"
+#include <stdarg.h>
+
+/**
+ * print_string - function that prints strings followed by newline
+ * @separator: is the string to be printed between the strings
+ * @n: number of strings passed to the function
+ * @...: ellipsis
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list nm;
+	unsigned int num;
+	char *s;
+
+	va_start(nm, n);
+
+	for (num = 0; num < n; num++)
+	{
+		s = va_arg(nm, char *);
+		if (s == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+			printf("%s", str);
+		if (num != (n - 1) && separator != NULL)
+			printf("%s", separator);
+
+	}
+	printf('\n');
+	va_end(strings);
+}
