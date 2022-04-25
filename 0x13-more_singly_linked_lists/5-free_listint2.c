a4dc37d53d7b63b117cb38f144434aaba3efa91f
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-04-25 20:16:03 +0000

    just work

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t
+ * @head: head double pointer
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	while (*head != NULL)
+	{
+		node = *head;
+		*head = head->next;
+		free(node);
+	}
+}
