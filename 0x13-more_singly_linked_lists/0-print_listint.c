932a6b9112d90f10b5e0956e0abcd83479545ba9
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-04-25 21:09:27 +0000

    fixing error

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * print_listint - a function that prints all elements of a listint_t
+ * print_listint - a function that prints all
+ * elements of a listint_t
  * @h: pointer to the node
  *
  * Return: the total number of nodes.
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t num_nodes = 0;
