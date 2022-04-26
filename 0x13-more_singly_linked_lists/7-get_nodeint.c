fa1838fccf56e8bac424cf9283c635dca2d8d7c0
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-04-26 09:32:06 +0000

    new commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,6 +11,7 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *node;
+	index = 0;
 
 	if (head == NULL)
 	{
@@ -22,6 +23,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (node != NULL && index)
 	{
 		node = node->next;
+		index++;
 	}
 	return (node);
 }
